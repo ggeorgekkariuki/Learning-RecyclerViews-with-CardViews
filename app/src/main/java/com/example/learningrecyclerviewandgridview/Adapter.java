fa7451c62e0f04bfecd49d8fedf383b0490db607
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-31 03:20:17 +0300

    The Adapter methods are overriden

--- app/src/main/java/com/example/learningrecyclerviewandgridview/Adapter.java
+++ app/src/main/java/com/example/learningrecyclerviewandgridview/Adapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -34,18 +36,25 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-
+//        This method binds the data by using the ViewHolder using the data stored at certain position
+        holder.mTextView.setText(titles.get(position));
+        holder.mImageView.setImageResource(images.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+//        The titles and images are placed in the same ViewHolder. We can count both as 1
+        return titles.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-//        It holds the views of one item
+//        It holds the views of one item which we can manipulate
+        TextView mTextView;
+        ImageView mImageView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTextView = itemView.findViewById(R.id.textView);
+            mImageView = itemView.findViewById(R.id.image);
         }
     }
 }
